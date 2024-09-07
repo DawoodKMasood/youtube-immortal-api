b2eba064fd8741f64ed847dc329a430cab3fb9cb
Dawood Masood <91176669+DawoodKMasood@users.noreply.github.com>
2024-09-08 03:34:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://immortals-cod-api.playwox.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
